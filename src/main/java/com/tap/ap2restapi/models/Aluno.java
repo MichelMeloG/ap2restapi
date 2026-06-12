0cf5aeacd288ea413a9bb2019704fe6a62dcb281
MichelMeloG <michelmg.dev145@gmail.com>
2026-06-11 23:34:12 -0300

    Corrige import faltando do AllArgsConstructor

--- src/main/java/com/tap/ap2restapi/models/Aluno.java
+++ src/main/java/com/tap/ap2restapi/models/Aluno.java
@@ -2,6 +2,7 @@ package com.tap.ap2restapi.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
